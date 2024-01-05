6009d737898bce3da44166b37ca492098845f299
Xu Bai <baixu.must@gmail.com>
2024-01-05 14:46:22 +0800

    Add comment to unicode.go

--- ch02/unicode.go
+++ ch02/unicode.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	const sL = "\x99\x00ab\x50\x00\x23\x50\x29\x9c"
-
+	// 使用这种方式其实是在遍历底层的byte数组，所以当我们将byte转化成rune的时候，只有ascii code能被成功转换
 	for i := 0; i < len(sL); i++ {
 		if unicode.IsPrint(rune(sL[i])) {
 			fmt.Printf("%c\n", sL[i])
